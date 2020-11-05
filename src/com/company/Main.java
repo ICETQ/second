92ad8a73896e98ba6b52125bd4a6528cc58437a8
Askar <ggggthingg@gmail.com>
2020-11-05 14:47:02 -0600

    master2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,7 @@ public class Main {
         System.out.println("first commit");
     }
     
-    public void h1(){
+    public void h1(int dig){
+        System.out.println("digit: = "+dig);
     }
 }
